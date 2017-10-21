56a603c84ae306830062956a64dfba90225f562f
NguyenVanLam260398 <lamnv260398@gmail.com>
2017-10-21 18:44:55 +0700

    da sua lai

--- Baitap.java
+++ Baitap.java
@@ -8,56 +8,44 @@ public class Baitap{
         VietNam Vn = new VietNam();
         Phap P = new Phap();
         Anh A = new Anh();
-        Vn.xuatV();
-        P.xuatP();
-        A.xuatA();
+        Vn.chao();
+        P.chao();
+        A.chao();
     }
 }
-class Goc{
+interface Goc{
+}
+class VietNam implements Goc{
     private String ten;
     private int tuoi;
-    public Goc(){
-       ten= "Null";
-       tuoi=0;
+    public VietNam(){
+        ten = "Lam";
+        tuoi = 20;
     }
-    public Goc(String TEN, int TUOI){
-        ten = TEN;
-        tuoi = TUOI;
-}
-    public String getTen(){
-        return ten;
-    }
-    public void setTen(String ten){
-        this.ten = ten;
-    }
-    public int GetTuoi(){
-        return tuoi;
-    }
-    public void setTuoi(int tuoi){
-        this.tuoi = tuoi;
+    public void chao(){
+      System.out.println("\n Xin chao "+ten+", ban "+tuoi);
     }
 }
-class VietNam extends Goc{
-    public void xuatV(){
-        VietNam vn = new VietNam();
-        vn.setTen("Lam");
-        vn.setTuoi(20);
-        System.out.println("xin chao"+vn.getTen()+",Ban "+vn.GetTuoi()+"tuoi");        
+class Phap implements Goc{
+    private String teN;
+    private int tuoI;
+    public Phap(){
+        teN = "Alex";
+        tuoI = 25;
+    }
+    public void chao(){
+        System.out.println("\n bonjour "+teN+", tu a "+tuoI+" ans ");
     }
 }
-class Phap extends Goc{
-    public void xuatP(){
-        Phap p = new Phap();
-        p.setTen("David");
-        p.setTuoi(15);
-        System.out.println("xin chao"+p.getTen()+",Ban "+p.GetTuoi()+"tuoi");        
+class Anh implements Goc{
+    private String Ten;
+    private int Tuoi;
+    public Anh(){
+        Ten = "David";
+        Tuoi = 15;
+    }
+    public void chao(){
+        System.out.println("\n Hello "+Ten+", you are "+Tuoi+" year olds ");
     }
 }
-class Anh extends Goc{
-    public void xuatA(){
-        Anh a = new Anh();
-        a.setTen("Lam");
-        a.setTuoi(20);
-        System.out.println("xin chao"+a.getTen()+",Ban "+a.GetTuoi()+"tuoi");        
-    }
-}
+
